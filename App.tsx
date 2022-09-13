b08cffdb1dc68f6286718a31afd03d63fb8383b6
JJ <joaoart42@gmail.com>
2022-09-13 12:30:28 -0300

    :tada: initial commit

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>
+        Hello World.
+      </Text> */}
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
+      <Button title='HELLO WORLD'/>
+      <StatusBar style="auto"/>
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
