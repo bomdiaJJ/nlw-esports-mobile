7ed7f333274788673a0ca64c698a3ddc1d51b843
JJ <joaoart42@gmail.com>
2022-09-18 18:28:02 -0300

    :sparkles: adiciona os cards de anúncios

--- src/screens/Game/styles.ts
+++ src/screens/Game/styles.ts
@@ -1,5 +1,50 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: 'space-between'
+  },
+  logo: {
+    width: 72,
+    height: 40
+  },
+  right: {
+    width: 20,
+    height: 20
+  },
+  cover: {
+    width: 360,
+    height: 160,
+    borderRadius: 8,
+    marginTop: 32
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+    alignItems: 'flex-start'
+  },
+  containerList: {
+    width: '100%'
+  },
+  emptyListText: {
+    color: THEME.COLORS.CAPTION_300,
+    fontSize: THEME.FONT_SIZE.LG,
+    textAlign: 'center',
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+  },
+  emptyListContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect,useState } from 'react';
 import { Image, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native'
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Background } from '../../components/Background';
@@ -12,6 +13,12 @@ import { styles } from './styles';
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({id, title, bannerUrl} : GameCardProps) {
+    navigation.navigate('game', {id, title, bannerUrl});
+  }
 
   useEffect(() => {
     fetch('http://192.168.15.81:3333/games')
@@ -37,7 +44,8 @@ export function Home() {
           keyExtractor={item => item.id}
           renderItem={({item}) => (
             <GameCard
-              data={item}
+            data={item}
+            onPress={() => handleOpenGame(item)}
             />
           )}
           horizontal
