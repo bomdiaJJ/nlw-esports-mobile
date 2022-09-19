9bfec44dc96fbd1df157f07cabadfa65dd530a08
JJ <joaoart42@gmail.com>
2022-09-18 21:27:14 -0300

    :rocket: finaliza o projeto nlw

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -11,19 +11,28 @@ import { THEME } from '../../theme';
 
 import { GameParams } from '../../@types/navigation';
 
-import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
+import { DuoMatch } from '../../components/DuoMatch';
+import { Background } from '../../components/Background';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
 export function Game() {
 
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.15.81:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord));
   }
 
   useEffect(() => {
@@ -65,11 +74,6 @@ export function Game() {
           title={game.title}
           subtitle='Conecte-se e comece a jogar!'
         />
-{/* 
-        <DuoCard
-        /> */}
-
-        
 
         <FlatList
           data={duos}
@@ -77,7 +81,7 @@ export function Game() {
           renderItem={({item}) => (
             <DuoCard
               data={item}
-              onConnect={() => {}}
+              onConnect={() => getDiscordUser(item.id)}
             />
           )}
           horizontal
@@ -91,6 +95,11 @@ export function Game() {
           )}
         />
 
+        <DuoMatch
+          onClose={() => setDiscordDuoSelected('') }
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+        />
       </SafeAreaView>
     </Background>
   );
